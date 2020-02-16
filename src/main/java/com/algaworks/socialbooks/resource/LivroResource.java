09db07e499bce0556bdfa27d9261a054a8c9e407
Mario <juniorm3@msn.com>
2020-02-16 18:32:57 -0300

    Salvando o recurso Livro a partir de um POST

--- src/main/java/com/algaworks/socialbooks/resource/LivroResource.java
+++ src/main/java/com/algaworks/socialbooks/resource/LivroResource.java
@@ -3,6 +3,7 @@ package com.algaworks.socialbooks.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,19 @@ import com.algaworks.socialbooks.domain.Livro;
 import com.algaworks.socialbooks.repository.LivrosRepository;
 
 @RestController
+@RequestMapping("/livros")
 public class LivroResource {
 	
 	@Autowired
 	private LivrosRepository LivrosRepository;
 	
-	@RequestMapping(value = "/livros", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Livro> listar() {
 		return LivrosRepository.findAll();		
 	}
 
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(@RequestBody Livro livro) {
+		LivrosRepository.save(livro);
+	}
 }
