874f58d1086fef671d9a5cffa050b593b7e5517b
Mario <juniorm3@msn.com>
2020-02-16 21:25:32 -0300

    Tratamento correto das respostas HTTP 404 e 201

--- src/main/java/com/algaworks/socialbooks/resource/LivroResource.java
+++ src/main/java/com/algaworks/socialbooks/resource/LivroResource.java
@@ -1,13 +1,17 @@
 package com.algaworks.socialbooks.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.socialbooks.domain.Livro;
 import com.algaworks.socialbooks.repository.LivrosRepository;
@@ -25,18 +29,29 @@ public class LivroResource {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro) {
-		LivrosRepository.save(livro);
+	public ResponseEntity<Void> salvar(@RequestBody Livro livro) {
+		livro = LivrosRepository.save(livro);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(livro.getId()).toUri();				
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Livro buscar(@PathVariable("id") Long id) {
-		return LivrosRepository.findById(id).get();
+	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
+		Livro livro = LivrosRepository.findOne(id);
+		
+		if(livro == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(livro); 
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public void deletar(@PathVariable("id") Long id ) {
-		LivrosRepository.deleteById(id);
+		LivrosRepository.delete(id);
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
--- src/test/java/com/algaworks/socialbooks/SocialbooksapiApplicationTests.java
+++ src/test/java/com/algaworks/socialbooks/SocialbooksapiApplicationTests.java
@@ -1,9 +1,14 @@
 package com.algaworks.socialbooks;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SocialbooksapiApplication.class)
+@WebAppConfiguration
 class SocialbooksapiApplicationTests {
 
 	@Test
